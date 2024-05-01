31025d9cf83c99b47a2b694e7fe2c86efdbd6114
Kennlf <kenn741q@stud.kea.dk>
2024-05-01 10:20:17 +0200

    first commit

---
+++ backend/TimeRegistration/src/test/java/timeregistration/TimeRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package timeregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
