f1d5e9b0fc21c019bcb34326525ef19777a39847
Søs <slys87@gmail.com>
2024-05-29 12:59:41 +0200

    Changed links in navbar

--- frontend/TimeRegistrationFE/src/components/NavBar/Navibar.jsx
+++ frontend/TimeRegistrationFE/src/components/NavBar/Navibar.jsx
@@ -12,11 +12,11 @@ const NaviBar = () =>{
         <Container>
         <Navbar fixed="top" expand="lg">
           <Container>
-            <Navbar.Brand href="#home">ACTO</Navbar.Brand>
+            <Navbar.Brand href="/">ACTO</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#home">Se arbejdstid</Nav.Link>
+                <Nav.Link href="show">Se arbejdstid</Nav.Link>
                 <Nav.Link href="register">Registrer tid</Nav.Link>
                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
