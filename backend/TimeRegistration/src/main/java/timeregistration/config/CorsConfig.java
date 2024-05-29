ddcab8cf4c99790633f4507462fb430d87c59e26
Søs <slys87@gmail.com>
2024-05-29 12:47:55 +0200

    Handled cors-conflict in backend and frontend

---
+++ backend/TimeRegistration/src/main/java/timeregistration/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package timeregistration.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("Http://localhost:5173", "Http://localhost:5174")
+                        .allowedMethods("GET", "POST", "PUT")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+
+            }
+        };
+    }
+}
