84b0b26d78551c02955285fa2f7aee7d1c88679f
Kennlf <kenn741q@stud.kea.dk>
2024-06-19 12:19:05 +0200

    added new function to Navbar

--- frontend/TimeRegistrationFE/src/components/NavBar/Navibar.jsx
+++ frontend/TimeRegistrationFE/src/components/NavBar/Navibar.jsx
@@ -21,7 +21,7 @@ const NaviBar = () =>{
                 <Nav.Link href="registerTime">Registrer tid</Nav.Link>
                 <Nav.Link href='Calendar'>Kalender</Nav.Link>
                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="showAllEmployees">showAllEmployees</NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.2">
                     Another action
                   </NavDropdown.Item>
